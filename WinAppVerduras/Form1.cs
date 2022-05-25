1b912aac437c1bc23f311a9a347b0966d37e34d9
Rodrigo32280 <edison-rodrigo01@hotmail.com>
2022-05-24 19:28:40 -0500

    v2 Juego de paneles

--- WinAppVerduras/Form1.cs
+++ WinAppVerduras/Form1.cs
@@ -17,9 +17,24 @@ namespace WinAppVerduras
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private Form formularioActivo = null;
+        private void AbrirFormulariosHijos(Form formularioHijo)
         {
-
+            if (formularioActivo != null)
+            {
+                formularioActivo.Close();
+            }
+            formularioActivo = formularioHijo;
+            formularioHijo.TopLevel = false;
+            formularioHijo.FormBorderStyle = FormBorderStyle.None;
+            formularioHijo.Dock = DockStyle.Fill;
+            pnlContenedor.Controls.Add(formularioHijo);
+            pnlContenedor.Tag = formularioHijo;
+            formularioHijo.BringToFront();
+            formularioHijo.Show();
         }
+
+  
+       
     }
 }
