64ef03ad0a179d8803ac29ad7492a23e5f2ae8bd
Carlos Yuquilema <cayuquilema@gmail.com>
2022-05-24 19:48:19 -0500

    v6

--- WinAppVerduras/Form1.Designer.cs
+++ WinAppVerduras/Form1.Designer.cs
@@ -29,12 +29,6 @@
         private void InitializeComponent()
         {
             this.pnlLateral = new System.Windows.Forms.Panel();
-            this.pnlPie = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.pnlContenedor = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
             this.btnOtros = new System.Windows.Forms.Button();
@@ -42,10 +36,19 @@
             this.btnEAlternativas = new System.Windows.Forms.Button();
             this.btnESecuenciales = new System.Windows.Forms.Button();
             this.pnlLogo = new System.Windows.Forms.Panel();
+            this.pnlPie = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pnlContenedor = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rjCircularPictureBox1 = new WinAppVerduras.RJCircularPictureBox();
+            this.lblAdmin = new System.Windows.Forms.Label();
             this.pnlLateral.SuspendLayout();
             this.pnlPie.SuspendLayout();
             this.pnlContenedor.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rjCircularPictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlLateral
@@ -65,76 +68,6 @@
             this.pnlLateral.Name = "pnlLateral";
             this.pnlLateral.Size = new System.Drawing.Size(133, 700);
             this.pnlLateral.TabIndex = 6;
-            // 
-            // pnlPie
-            // 
-            this.pnlPie.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(34)))), ((int)(((byte)(54)))));
-            this.pnlPie.Controls.Add(this.button2);
-            this.pnlPie.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlPie.Location = new System.Drawing.Point(133, 0);
-            this.pnlPie.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pnlPie.Name = "pnlPie";
-            this.pnlPie.Size = new System.Drawing.Size(816, 87);
-            this.pnlPie.TabIndex = 7;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(47)))), ((int)(((byte)(61)))));
-            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(917, 0);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(32, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "X";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // pnlContenedor
-            // 
-            this.pnlContenedor.BackColor = System.Drawing.Color.DarkGray;
-            this.pnlContenedor.Controls.Add(this.label2);
-            this.pnlContenedor.Controls.Add(this.label3);
-            this.pnlContenedor.Controls.Add(this.pictureBox1);
-            this.pnlContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlContenedor.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.pnlContenedor.Location = new System.Drawing.Point(133, 87);
-            this.pnlContenedor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pnlContenedor.Name = "pnlContenedor";
-            this.pnlContenedor.Size = new System.Drawing.Size(816, 613);
-            this.pnlContenedor.TabIndex = 8;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(169, 23);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(431, 115);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Escuela Superior Politécnica de Chimborazo\r\nFacultad de Informática y Electrónica" +
-    "\r\nCarrera de Ingeniería en Software\r\n \r\n\r\n";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(333, 382);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(124, 92);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "\r\n10/05/2022\r\n \r\n\r\n";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(283, 128);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(229, 212);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
@@ -262,6 +195,104 @@
             this.pnlLogo.Size = new System.Drawing.Size(133, 121);
             this.pnlLogo.TabIndex = 0;
             // 
+            // pnlPie
+            // 
+            this.pnlPie.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(34)))), ((int)(((byte)(54)))));
+            this.pnlPie.Controls.Add(this.lblAdmin);
+            this.pnlPie.Controls.Add(this.rjCircularPictureBox1);
+            this.pnlPie.Controls.Add(this.button2);
+            this.pnlPie.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlPie.Location = new System.Drawing.Point(133, 0);
+            this.pnlPie.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlPie.Name = "pnlPie";
+            this.pnlPie.Size = new System.Drawing.Size(816, 87);
+            this.pnlPie.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(47)))), ((int)(((byte)(61)))));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(917, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // pnlContenedor
+            // 
+            this.pnlContenedor.BackColor = System.Drawing.Color.DarkGray;
+            this.pnlContenedor.Controls.Add(this.label2);
+            this.pnlContenedor.Controls.Add(this.label3);
+            this.pnlContenedor.Controls.Add(this.pictureBox1);
+            this.pnlContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContenedor.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pnlContenedor.Location = new System.Drawing.Point(133, 87);
+            this.pnlContenedor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlContenedor.Name = "pnlContenedor";
+            this.pnlContenedor.Size = new System.Drawing.Size(816, 613);
+            this.pnlContenedor.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(169, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(431, 115);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Escuela Superior Politécnica de Chimborazo\r\nFacultad de Informática y Electrónica" +
+    "\r\nCarrera de Ingeniería en Software\r\n \r\n\r\n";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(333, 382);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 92);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "\r\n10/05/2022\r\n \r\n\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(283, 128);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(229, 212);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rjCircularPictureBox1
+            // 
+            this.rjCircularPictureBox1.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
+            this.rjCircularPictureBox1.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.rjCircularPictureBox1.BorderColor2 = System.Drawing.Color.HotPink;
+            this.rjCircularPictureBox1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.rjCircularPictureBox1.BorderSize = 2;
+            this.rjCircularPictureBox1.GradientAngle = 50F;
+            this.rjCircularPictureBox1.Location = new System.Drawing.Point(614, 12);
+            this.rjCircularPictureBox1.Name = "rjCircularPictureBox1";
+            this.rjCircularPictureBox1.Size = new System.Drawing.Size(58, 58);
+            this.rjCircularPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rjCircularPictureBox1.TabIndex = 7;
+            this.rjCircularPictureBox1.TabStop = false;
+            // 
+            // lblAdmin
+            // 
+            this.lblAdmin.AutoSize = true;
+            this.lblAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdmin.ForeColor = System.Drawing.Color.White;
+            this.lblAdmin.Location = new System.Drawing.Point(689, 30);
+            this.lblAdmin.Name = "lblAdmin";
+            this.lblAdmin.Size = new System.Drawing.Size(61, 20);
+            this.lblAdmin.TabIndex = 8;
+            this.lblAdmin.Text = "Admin";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -271,14 +302,16 @@
             this.Controls.Add(this.pnlPie);
             this.Controls.Add(this.pnlLateral);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.pnlLateral.ResumeLayout(false);
             this.pnlPie.ResumeLayout(false);
+            this.pnlPie.PerformLayout();
             this.pnlContenedor.ResumeLayout(false);
             this.pnlContenedor.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rjCircularPictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -299,6 +332,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblAdmin;
+        private RJCircularPictureBox rjCircularPictureBox1;
     }
 }
 
